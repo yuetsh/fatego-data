c3735aa2c7dbe5821a65dc574540cc99ba02dff1
yuetsh <517252939@qq.com>
2019-07-19 18:38:31 +0800

    fix

--- main.go
+++ main.go
@@ -9,7 +9,8 @@ import (
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/geziyor/geziyor"
-	"github.com/geziyor/geziyor/exporter"
+	"github.com/geziyor/geziyor/client"
+	"github.com/geziyor/geziyor/export"
 )
 
 // File 文件名
@@ -49,44 +50,44 @@ func main() {
 }
 
 func fetchServants() {
-	geziyor.NewGeziyor(geziyor.Options{
+	geziyor.NewGeziyor(&geziyor.Options{
 		StartURLs: []string{"http://wiki.joyme.com/fgo/%E8%8B%B1%E7%81%B5%E5%88%97%E8%A1%A8"},
 		UserAgent: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.80 Safari/537.36",
-		Exporters: []geziyor.Exporter{exporter.JSONExporter{
-			FileName: File,
-		}},
-		ParseFunc: func(r *geziyor.Response) {
-			r.DocHTML.Find("#CardSelectTr > tbody").Children().Each(func(i int, s *goquery.Selection) {
-				if i == 0 {
-					return
-				}
-				// 基本信息
-				id := s.Find("td:nth-child(1)")
-				link := s.Find("td:nth-child(3) > a")
-				star := s.Find("td:nth-child(4)")
-				class := s.Find("td:nth-child(5)")
-				url, _ := link.Attr("href")
-
-				servant := Servant{
-					ID:    strings.TrimSpace(id.Text()),
-					Name:  link.Text(),
-					Link:  "http://wiki.joyme.com" + url,
-					Star:  strings.TrimSpace(star.Text()),
-					Class: strings.TrimSpace(class.Text()),
-					Image: make(map[string]string),
-				}
-				// 详细信息
-				r.Geziyor.Get(servant.Link, func(r *geziyor.Response) {
-					table := r.DocHTML.Find("#mw-content-text > div:nth-child(4) > div.col-md-4 > table")
-					table.Find("img").Each(func(i int, s *goquery.Selection) {
-						url, _ := s.Attr("src")
-						name, _ := s.Attr("data-file-name")
-						parsedName := strings.ReplaceAll(name, ".png", "")
-						servant.Image[parsedName] = strings.ReplaceAll(url, "/dr/300__", "")
-					})
-				})
-				r.Exports <- servant
-			})
-		},
+		Exporters: []export.Exporter{&export.JSONLine{FileName: File}},
+		ParseFunc: findDetail,
 	}).Start()
+}
+
+func findDetail(g *geziyor.Geziyor, r *client.Response) {
+	r.HTMLDoc.Find("#CardSelectTr > tbody").Children().Each(func(i int, s *goquery.Selection) {
+		if i == 0 {
+			return
+		}
+		// 基本信息
+		id := s.Find("td:nth-child(1)")
+		link := s.Find("td:nth-child(3) > a")
+		star := s.Find("td:nth-child(4)")
+		class := s.Find("td:nth-child(5)")
+		url, _ := link.Attr("href")
+
+		servant := Servant{
+			ID:    strings.TrimSpace(id.Text()),
+			Name:  link.Text(),
+			Link:  "http://wiki.joyme.com" + url,
+			Star:  strings.TrimSpace(star.Text()),
+			Class: strings.TrimSpace(class.Text()),
+			Image: make(map[string]string),
+		}
+		// 详细信息
+		g.Get(servant.Link, func(g *geziyor.Geziyor, r *client.Response) {
+			table := r.HTMLDoc.Find("#mw-content-text > div:nth-child(4) > div.col-md-4 > table")
+			table.Find("img").Each(func(i int, s *goquery.Selection) {
+				url, _ := s.Attr("src")
+				name, _ := s.Attr("data-file-name")
+				parsedName := strings.ReplaceAll(name, ".png", "")
+				servant.Image[parsedName] = strings.ReplaceAll(url, "/dr/300__", "")
+			})
+			g.Exports <- servant
+		})
+	})
 }
